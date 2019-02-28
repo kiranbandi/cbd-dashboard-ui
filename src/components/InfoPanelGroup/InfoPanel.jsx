bb5bc27f30dd562c65ca28c8f8efa30ef58c3f4f
Venkat Bandi <venkat.bandi@usask.ca>
2019-02-28 14:58:34 -0600

    invalid bug date fix

--- src/components/InfoPanelGroup/InfoPanel.jsx
+++ src/components/InfoPanelGroup/InfoPanel.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import moment from 'moment';
 import RotationSchedule from './RotationSchedule';
 import EPASpeedInfo from './EPASpeedInfo';
 
