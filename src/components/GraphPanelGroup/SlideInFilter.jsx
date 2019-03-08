9ebe06768833dd2f92b8ebcde0e238f947346ca5
Venkat Bandi <venkat.bandi@usask.ca>
2019-03-08 14:32:45 -0600

    Fixed clear cache issue when builds are made to production

--- src/components/GraphPanelGroup/SlideInFilter.jsx
+++ src/components/GraphPanelGroup/SlideInFilter.jsx
@@ -6,30 +6,48 @@ export default class SlideInFilter extends Component {
 
     constructor(props) {
         super(props);
+        this.onSelectSubmit = this.onSelectSubmit.bind(this);
+    }
+
+    onSelectSubmit() {
+        //     const  residentFilter.startDate = document.getElementById('filter-demographic-cp') ? document.getElementById('filter-demographic-cp').value : '',
+        //     residentFilter.endDate = document.getElementById('filter-demographic-cp') ? document.getElementById('filter-demographic-dm').value : '';
     }
 
 
-    createSelect(label, optionArray = []) {
+    createSelect(label, optionArray = [], filterKey, records) {
+
+        const optionCountMap = _.times(optionArray.length, () => 0);
+
+        _.map(records, (record) => {
+            const context = record.Situation_Context.split(",").map((val_0)=>val_0.trim());
+            debugger;
+        })
+
+
         return <div className='demographic-box inner-filter-box'>
             <label className='filter-label'>{label}</label>
             {optionArray.length > 0 ?
-                <select id='filter-demographic' defaultValue={''} className="custom-select">
+                <select id={'filter-demographic-' + filterKey} defaultValue={''} className="custom-select">
                     {_.map(optionArray, (val, index) => { return <option key={index} value={val}> {val}</option> })}
                 </select> :
                 <span className='no-option-text'>N/A</span>}
         </div>
+
+
+
     }
 
     render() {
 
-        const { innerKey, epaSource, width } = this.props,
+        const { innerKey, epaSource, width, data = [] } = this.props,
             { clinicalPresentation, patientDemographic } = templateEpaSourceMap[innerKey];
 
 
         return (
             <div className='filter-box' style={{ width: (width * 4) - 75 }}>
-                {this.createSelect('Clinical Presentation', clinicalPresentation[epaSource])}
-                {this.createSelect('Demographic', patientDemographic[epaSource])}
+                {this.createSelect('Clinical Presentation', clinicalPresentation[epaSource], 'cp', data)}
+                {this.createSelect('Demographic', patientDemographic[epaSource], 'dm', data)}
                 <div className='inner-button-box'>
                     <button type="submit" className="btn btn-primary-outline">HIGHLIGHT</button>
                 </div>
