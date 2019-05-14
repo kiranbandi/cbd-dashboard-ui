6af623b5a5a7978cce667a376fdb6514ee0f6815
Venkat Bandi <venkat.bandi@usask.ca>
2019-05-14 12:41:48 -0600

    fixed bounce back to home when no token is present

--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -5,6 +5,11 @@ var authorization = {};
 authorization.checkloginStatus = function(nextState, replace) {
     if (!sessionStorage.jwt) {
         toastr["error"]("Please Login to view the requested page", "Authorization Error");
+        replace({
+            pathname: '/',
+            state: { nextPathname: nextState.location.pathname }
+        });
+
     }
 }
 
