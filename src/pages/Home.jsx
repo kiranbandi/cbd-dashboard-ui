cf3f84177ee8a17712ad3a0c96bca5391beaca23
kiranbandi <bvenkatkiran@gmail.com>
2019-06-11 12:42:23 -0600

    added youtube video link to resident dashboard demonstration on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 //  Image url handling is convoluted in scss , much easier to set inline and get images from root
 let backgroundStyle = { background: 'url(assets/img/background.png)' };
+import YouTube from 'react-youtube';
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+
+    //200px to offset the margin on both sides and vertical scroll bar width
+    let widthOfPage = document.body.getBoundingClientRect().width;
+
+    if (widthOfPage > 1170) {
+      widthOfPage = 1000
+    }
+    else if (widthOfPage < 700) {
+      widthOfPage = 0.90 * widthOfPage;
+    }
+    else {
+      widthOfPage = 0.75 * widthOfPage;
+    }
+
+
     return (
       <div>
 
@@ -20,7 +41,16 @@ class Home extends Component {
           <p>These dashboards have been developed as part of a research project to determine the essential elements for presenting Competency-Based Medical Education dashboards. They display data from Competency By Design<a href="http://www.royalcollege.ca/rcsite/cbd/competence-by-design-cbd-e">(CBD)</a> assessment systems in intuitive ways that support the needs of learners, teachers, and program administrators.</p>
           <h1> Where is the data coming from ?</h1>
           <p>The data displayed on the dashboard has been compiled from assessment forms filled by physician observers based upon observations of EPAs(Entrustable Professional Activities) that are characteristic of each learners stage of training and discipline.</p>
+
+          <h1> Resident Dashboard Demonstration</h1>
+          <YouTube
+            containerClassName='youtube-container'
+            videoId="hACPB0YyOyA"
+            opts={{ width: widthOfPage, height: widthOfPage / 2 }} />
+
+
         </div>
+
       </div>
 
     )
