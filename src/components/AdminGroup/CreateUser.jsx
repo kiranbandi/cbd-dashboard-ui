2d5e97b31a721435b294745e58fc669629707527
kiranbandi <bvenkatkiran@gmail.com>
2019-07-02 12:20:08 -0600

    updating backend to nginx instead of apache

--- src/components/AdminGroup/CreateUser.jsx
+++ src/components/AdminGroup/CreateUser.jsx
@@ -7,7 +7,7 @@ import rotationScheduleMap from '../../utils/rotationScheduleMap';
 
 const possibleAcademicYears = _.keys(rotationScheduleMap);
 
-const possibleRotations = ["EM", "EM(REGINA)", "EM(PED)", "EM(RGNL)", "ANESTHESIA", "CARDIO", "ICU", "GIM", "GEN SURG", "NEURO", "OPTHO", "ORTHO", "PLASTICS", "SELECTIVE", "TOXICOLOGY", "TRAUMA", "OTHER"];
+const possibleRotations = ["EM", "EM(REGINA)", "EM(PED)", "EM(RGNL)", "ANESTHESIA", "CARDIO", "ICU", "GIM", "GEN SURG", "NEURO", "OPTHO", "ORTHO", "PLASTICS", "SELECTIVE", "TOXICOLOGY", "TRAUMA", "OBS/GYN", "OTHER"];
 const possiblePhases = ['transition-to-discipline', 'foundations-of-discipline', 'core-of-discipline', 'transition-to-practice'];
 
 export default class CreateUser extends Component {
