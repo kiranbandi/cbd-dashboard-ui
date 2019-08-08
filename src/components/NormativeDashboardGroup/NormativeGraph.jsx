6534b55bd1e959c06d76b9c2f7aaca95ee8d9716
kiranbandi <bvenkatkiran@gmail.com>
2019-08-07 22:18:05 -0600

    major normative dashboard upgrade

--- src/components/NormativeDashboardGroup/NormativeGraph.jsx
+++ src/components/NormativeDashboardGroup/NormativeGraph.jsx
@@ -7,9 +7,15 @@ export default class NormativeGraph extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            trackType: 'epa_per_week'
+            trackType: 'record_count',
+            dualTracks: false
         };
         this.radioChange = this.radioChange.bind(this);
+        this.onCheckboxChange = this.onCheckboxChange.bind(this);
+    }
+
+    onCheckboxChange() {
+        this.setState({ dualTracks: !this.state.dualTracks });
     }
 
 
@@ -19,23 +25,54 @@ export default class NormativeGraph extends Component {
 
     render() {
 
-        const { records, width } = this.props, { trackType } = this.state;
+        const { records, width } = this.props, { trackType, dualTracks } = this.state;
 
-        let sortedRecords = _.sortBy(records, (d) => d[trackType]);
+        const dateFilterActive = document.getElementById('filter-dateFilterActive') && document.getElementById('filter-dateFilterActive').checked;
+
+        let sortedRecords = _.reverse(_.sortBy(records, (d) => d[trackType]));
+
+        let datasets = [{
+            label: "Overall",
+            fillColor: "rgba(28,168,221,.03)",
+            strokeColor: "#43b98e",
+            pointColor: "#43b98e",
+            pointStrokeColor: 'rgba(28,168,221,.03)',
+            pointHighlightFill: "rgba(28,168,221,.03)",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: _.map(sortedRecords, (d) => d[trackType])
+        }];
+
+        if (dateFilterActive) {
+            if (dualTracks) {
+                datasets.push({
+                    label: "Period",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: _.map(sortedRecords, (d) => d[trackType + "_period"])
+                });
+            }
+            else {
+                datasets = [{
+                    label: "Period",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: _.map(sortedRecords, (d) => d[trackType + "_period"])
+                }];
+            }
+        }
 
         // create line object for chart
         const lineData = {
             labels: _.map(sortedRecords, (d) => d.resident_name),
-            datasets: [{
-                label: "Normative",
-                fillColor: "rgba(28,168,221,.03)",
-                strokeColor: "#43b98e",
-                pointColor: "#43b98e",
-                pointStrokeColor: 'rgba(28,168,221,.03)',
-                pointHighlightFill: "rgba(28,168,221,.03)",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: _.map(sortedRecords, (d) => d[trackType])
-            }]
+            datasets
         }
 
         let lineOptions = {
@@ -44,22 +81,32 @@ export default class NormativeGraph extends Component {
 
         return (
             <div className='normative-graph'>
-                <div className='radio-button-container'>
-                    <RadioButton value={'epa_per_week'} id={'track_epa_per_week'} className='track-radio' name='track-select'
-                        label={"EPAs/week"}
-                        onChange={this.radioChange}
-                        checked={trackType == 'epa_per_week'} />
-
-                    <RadioButton value={'record_count'} id={'track_record_count'} className='track-radio' name='track-select'
-                        label={"EPAs"}
-                        onChange={this.radioChange}
-                        checked={trackType == 'record_count'} />
-                    <RadioButton value={'expired'} id={'track_expired'} className='track-radio' name='track-select'
-                        label={"Expired"}
-                        onChange={this.radioChange}
-                        checked={trackType == 'expired'} />
+                <div className='sub-filter'>
+                    <div className='radio-button-container'>
+                        <RadioButton value={'record_count'} id={'track_record_count'} className='track-radio' name='track-select'
+                            label={"EPAs"}
+                            onChange={this.radioChange}
+                            checked={trackType == 'record_count'} />
+                        <RadioButton value={'epa_per_week'} id={'track_epa_per_week'} className='track-radio' name='track-select'
+                            label={"EPAs/week"}
+                            onChange={this.radioChange}
+                            checked={trackType == 'epa_per_week'} />
+                        <RadioButton value={'expired'} id={'track_expired'} className='track-radio' name='track-select'
+                            label={"Expired"}
+                            onChange={this.radioChange}
+                            checked={trackType == 'expired'} />
+                    </div>
+                    {dateFilterActive &&
+                        <div className="checkbox custom-control text-center custom-checkbox">
+                            <label className='filter-label'>
+                                {"Compare with Overall"}
+                                <input id='filter-dateFilterActive' type="checkbox" checked={dualTracks} onChange={this.onCheckboxChange} />
+                                <span className="custom-control-indicator"></span>
+                            </label>
+                        </div>}
                 </div>
                 <Line
+                    redraw={true}
                     options={lineOptions}
                     data={lineData}
                     width={width} height={450} />
