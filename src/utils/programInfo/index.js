4036a951b1e81b8e0f6a629c530cfdbec1f73663
kiranbandi <bvenkatkiran@gmail.com>
2019-08-22 20:16:39 -0600

    moved list of all programs to a indepnt file and added base file for Internal Medicine

--- src/utils/programInfo/index.js
+++ src/utils/programInfo/index.js
@@ -2,9 +2,10 @@ import { default as EM } from './programs/EM';
 import { default as ANESTHESIA } from './programs/ANESTHESIA';
 import { default as OBGYN } from './programs/OBGYN';
 import { default as PATH } from './programs/PATH';
+import { default as IM } from './programs/IM';
 
 // populate program info with multiple department information and export its
-const PROGRAM_INFO = { EM, ANESTHESIA, OBGYN, PATH };
+const PROGRAM_INFO = { EM, ANESTHESIA, OBGYN, PATH, IM };
 
 export { PROGRAM_INFO };
 export { default as ROTATION_SCHEDULE_MAP }
@@ -13,3 +14,5 @@ export { default as CARDS_LIST }
 from './CARDS_LIST';
 export { default as PHASES_LIST }
 from './PHASES_LIST';
+export { default as PROGRAM_LIST }
+from './PROGRAM_LIST';
