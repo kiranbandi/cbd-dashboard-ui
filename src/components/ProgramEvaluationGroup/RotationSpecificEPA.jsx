1c855442a28f59aa52f701ecd701a7b1b87885ee
kiranbandi <bvenkatkiran@gmail.com>
2019-08-22 19:25:11 -0600

    updated pathology clinical presentation filters

--- src/components/ProgramEvaluationGroup/RotationSpecificEPA.jsx
+++ src/components/ProgramEvaluationGroup/RotationSpecificEPA.jsx
@@ -6,7 +6,6 @@ export default class RotationSpecificEPA extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
             selectedRotation: { 'label': '', 'option': '' }
         };
