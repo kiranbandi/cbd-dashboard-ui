b50e9046474ec1380102b31dc842e2ba2df10b0d
kiranbandi <bvenkatkiran@gmail.com>
2019-08-22 20:38:41 -0600

    updated recent EPA chart points to turn into diamonds when they lie in the selected date range

--- src/utils/programInfo/PROGRAM_LIST.js
+++ src/utils/programInfo/PROGRAM_LIST.js
@@ -1,6 +1,6 @@
 export default [{ value: 'EM', label: 'Emergency Medicine' },
     { value: 'PATH', label: 'Pathology & Lab Medicine' },
     { value: 'OBGYN', label: 'Obstetrics and Gynecology' },
-    { value: 'IM', label: 'Internal Medicine' },
-    { value: 'ANESTHESIA', label: 'Anesthesia' }
+    { value: 'IM', label: 'Internal Medicine (In Progress)' },
+    { value: 'ANESTHESIA', label: 'Anesthesia (In Progress)' }
 ];
