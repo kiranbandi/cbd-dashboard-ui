108387a38d88f1890c62bc21f5387ee97bf23b9a
kiranbandi <bvenkatkiran@gmail.com>
2019-08-22 19:25:11 -0600

    updated pathology clinical presentation filters

--- src/utils/programInfo/programs/PATH.js
+++ src/utils/programInfo/programs/PATH.js
@@ -37,7 +37,7 @@ const programInfo = {
             },
             maxObservation: {
                 "1.1": 13,
-                "1.2": 15,
+                "1.2": 16,
                 "1.3": 2,
                 "1.4": 1
             },
@@ -74,29 +74,36 @@ const programInfo = {
             },
             maxObservation: {
                 "2.1": 10,
-                "2.2": 2,
+                "2.2": 16,
                 "2.3": 10,
                 "2.4": 6,
                 "2.5": 10,
                 "2.6": 2
             },
             clinicalPresentation: {
-                '2.1': [],
-                '2.2': [],
-                '2.3': [],
-                '2.4': [],
-                '2.5': [],
-                '2.6': []
+                '2.1': ["direct observation of history", "direct observation of communication with patients", "case discussion or chart review - Setting: medicine", "surgery", "oncology", "pediatrics", "other"],
+                '2.2': ["open the pulmonary vasculature", "open the aorta, identify and dissect the main arterial branches of the aorta", "open the bowel", "dissect the pelvic block", "obtain quality photographs as directed", "complete required forms for ancillary tests", "other"],
+                '2.3': ["direct", "indirect"],
+                '2.4': ["tissue", "blood", "microbiological sample", "other"],
+                '2.5': ["breast", "bone & soft tissue", "gynecology", "gastrointestinal", "genitourinary", "head & neck", "endocrine", "skin", "lymph nodes", "placenta"],
+                '2.6': ["bone marrow", "Fine Needle Aspirate (FNA)"]
             },
             patientDemographic: {
-                '2.1': [],
+                '2.1': ["medicine", "surgery", "oncology", "pediatrics"],
                 '2.2': [],
                 '2.3': [],
-                '2.4': [],
+                '2.4': ["immunohistochemistry", "cytogenetics", "molecular", "in situ hybridization", "immunofluorescence", "flow cytometry", "electron microscopy"],
                 '2.5': [],
-                '2.6': []
+                '2.6':  ["surgical", "cytology", "hematopathology", "microbiology", "general"]
             },
-            filterTitles: {}
+            filterTitles: {
+                '2.1': ["Observation Type", "Setting"],
+                '2.2': ["Task", ""],
+                '2.3': ["Observation Type", ""],
+                '2.4': ["Specimen Type", "Ancillary tests"],
+                '2.5': ["Organ System or Tissue", ""],
+                '2.6': ["Procedure", "Specimen Type"]
+            }
         },
         3: {
             'ID': 'CORE',
@@ -126,55 +133,55 @@ const programInfo = {
                 "3.22": "Conducting scholarly work"
             },
             maxObservation: {
-                "3.1": 6,
-                "3.2": 3,
-                "3.3": 6,
-                "3.4": 50,
+                "3.1": 13,
+                "3.2": 6,
+                "3.3": 12,
+                "3.4": 150,
                 "3.5": 70,
                 "3.6": 70,
                 "3.7": 10,
                 "3.8": 5,
-                "3.9": 2,
-                "3.10": 12,
+                "3.9": 14,
+                "3.10": 34,
                 "3.11": 5,
                 "3.12": 10,
-                "3.13": 5,
+                "3.13": 15,
                 "3.14": 25,
-                "3.15": 5,
+                "3.15": 45,
                 "3.16": 10,
                 "3.17": 1,
                 "3.18": 5,
                 "3.19": 3,
                 "3.20": 10,
-                "3.21": 10,
+                "3.21": 4,
                 "3.22": 1
             },
             clinicalPresentation: {
-                '3.1':  [],
-                '3.2': [],
-                '3.3': [],
-                '3.4': [],
-                '3.5': [],
-                '3.6': [],
+                '3.1':  ["complex", "routine"],
+                '3.2': ["fetal", "neonatal", "pediatric"],
+                '3.3': ["natural", "accidental", "suicide", "undetermined"],
+                '3.4': ["breast", "bone & soft tissue", "placenta", "cardiovascular", "endocrine", "neuropathology", "gynecology", "gastrointestinal", "genitourinary", "head & neck", "lymph nodes or spleen", "respiratory", "skin"],
+                '3.5': ["breast", "bone & soft tissue", "cardiovascular", "endocrine", "gastrointestinal", "genitourinary", "gynecology", "head & neck", "lymph nodes/spleen", "neuropathology", "placenta", "skin", "thoracic"],
+                '3.6': ["breast", "bone & soft tissue", "cardiovascular", "endocrine", "gastrointestinal", "genitourinary", "gynecology", "head & neck", "lymph nodes/spleen", "neuropathology", "placenta", "skin", "thoracic"],
                 '3.7': [],
                 '3.8': [],
-                '3.9': [],
-                '3.10': [],
-                '3.11': [],
-                '3.12': [],
-                '3.13':  [],
-                '3.14': [],
-                '3.15': [],
-                '3.16': [],
-                '3.17': [],
-                '3.18': [],
+                '3.9':  ["bacteriology", "virology", "parasitology", "mycology", "serology"],
+                '3.10': ["non-neoplastic", "myeloproliferative", "myelodysplastic", "lymphoproliferative", "reactive/infectious", "Hodgkin lymphoma", "Non-Hodgkin lymphoma", "other"],
+                '3.11': ["normal", "hemoglobinopathy", "enzymopathy", "membranopathy"],
+                '3.12': ["bleeding", "thrombosis", "drug monitoring", "asymptomatic", "abnormal lab value"],
+                '3.13':  ["blood component", "blood product"],
+                '3.14': ["formalin fixed paraffin embedded (FFPE) tissue", "fresh/frozen tissue", "blood/bone marrow", "other"],
+                '3.15': ["gynecological", "FNA", "fluid (pleural, peritoneal, urine, CSF etc.)", "endoscopic ultrasound (EUS)", "endobronchial ultrasound (EBUS)"],
+                '3.16': ["anatomical pathology", "hematopathology", "medical microbiology", "medical biochemistry"],
+                '3.17': ["clinical utility", "analytical (total allowable error/precision/reference range, etc)", "financial", "setting up QC", "investigating proficiency testing options", "other"],
+                '3.18': ["clinical consult", "simulation", "laboratory initiated"],
                 '3.19': [],
-                '3.20': [],
+                '3.20': ["serum protein electrophoresis", "urine protein electrophoresis", "joint crystal", "urine crystal", "therapeutic drug monitoring", "clinical toxicology", "other"],
                 '3.21': [],
                 '3.22': []
             },
             patientDemographic: {
-                '3.1':  [],
+                '3.1':  ["full", "limited or focused autopsy"],
                 '3.2': [],
                 '3.3': [],
                 '3.4': [],
@@ -182,22 +189,40 @@ const programInfo = {
                 '3.6': [],
                 '3.7': [],
                 '3.8': [],
-                '3.9': [],
+                '3.9': ["blood", "CSF", "urine", "other body fluids", "stool", "genital", "respiratory", "wound/skin swab", "surgical/tissue specimen"],
                 '3.10': [],
                 '3.11': [],
-                '3.12': [],
-                '3.13':  [],
-                '3.14': [],
-                '3.15': [],
-                '3.16': [],
+                '3.12': ["platelet disorder", "factor disorder", "vascular disorder", "multifactorial"],
+                '3.13':  ["clinical", "laboratory", "collection", "storage", "modification", "distribution", "utilization/stewardship", "surgical indication", "medical indication", "trauma", "platelet disorder", "factor disorder", "immunomodulation", "other"],
+                '3.14': ["Polymerase Chain Reaction(PCR)", "next generation sequencing (NGS)", "karyotype", "fluorescence in situ hybridization (FISH) /chromogenic in situ hybridization (CISH)", "ploidy"],
+                '3.15': ["direct", "indirect"],
+                '3.16': ["routine monitoring", "error", "complaint", "other"],
                 '3.17': [],
-                '3.18': [],
+                '3.18': ["anatomical pathology", "hematopathology", "medical microbiology", "medical biochemistry"],
                 '3.19': [],
                 '3.20': [],
                 '3.21': [],
                 '3.22': []
             },
-            filterTitles: {}
+            filterTitles: {
+                '3.1':  ["Case complexity", "case details"],
+                '3.2': ["Type", ""],
+                '3.3': ["Manner of death", ""],
+                '3.4': ["Organ system or tissue", ""],
+                '3.5': ["Organ system or tissue", ""],
+                '3.6': ["Organ system or tissue", ""],
+                '3.9': ["Microorganism", "Specimen type"],
+                '3.10': ["Category", ""],
+                '3.11': ["Category", ""],
+                '3.12': ["Presentation", "Category"],
+                '3.13':  ["Inventory", "Category"],
+                '3.14': ["Specimen type", "Test type"],
+                '3.15': ["Specimen type", "Observation Type"],
+                '3.16': ["Lab discipline", "Trigger for review"],
+                '3.17': ["Modality", ""],
+                '3.18': ["Category", "Lab discipline"],
+                '3.20': ["Specimen type", ""]
+            }
         },
         4: {
             'ID': 'TP',
@@ -215,9 +240,9 @@ const programInfo = {
                 "4.4": 1
             },
             clinicalPresentation: {
-                '4.1': [],
-                '4.2': [],
-                '4.3': [],
+                '4.1':  ["surgical pathology", "cytopathology", "autopsy pathology", "biochemistry", "microbiology", "hematopathology", "transfusion medicine", "hemostasis and coagulation", "molecular pathology"],
+                '4.2':  ["anatomical pathology", "biochemistry", "microbiology", "hematopathology"],
+                '4.3':  ["anatomical pathology", "biochemistry", "microbiology", "hematopathology"],
                 '4.4': []
             },
             patientDemographic: {
@@ -226,7 +251,12 @@ const programInfo = {
                 '4.3': [],
                 '4.4': []
             },
-            filterTitles: {}
+            filterTitles: {
+                '4.1': ["Lab Discipline", ""],
+                '4.2': ["Lab Discipline", ""],
+                '4.3': ["Lab Discipline", ""],
+                '4.4': ["", ""]
+            }
         }
     }
 };
