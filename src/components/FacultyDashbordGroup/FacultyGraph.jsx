1eead62d63ffbdf655544aea24fd97a79d625e63
kiranbandi <bvenkatkiran@gmail.com>
2019-12-14 23:01:30 -0600

    faculty dashboard ability to highlight a particular faculty added

--- src/components/FacultyDashbordGroup/FacultyGraph.jsx
+++ src/components/FacultyDashbordGroup/FacultyGraph.jsx
@@ -15,54 +15,54 @@ export default class FacultyGraph extends Component {
         // 3) entrustment_score 
         // 4) words_per_comment
 
-        let { data, width, trackType, dateFilterActive, startDate, endDate } = this.props;
+        let { data, width, trackType, currentFaculty,
+            dateFilterActive, startDate, endDate } = this.props;
 
         // sort the data list based on the overall value
         data = data.sort((a, b) => a[1] - b[1]);
 
         const height = 350, margin = 10, Xoffset = 50;
 
+        const itemSize = data.length > 0 ? ((width - margin - Xoffset) / data.length) : 2;
+
 
         // create the X and Y scales and modify them based on the track type
-        const scaleX = d3.scaleLinear().range([Xoffset, width - margin]).domain([0, data.length - 1]);
+        const scaleX = d3.scaleLinear().range([Xoffset, width - margin - Xoffset]).domain([0, data.length - 1]);
         let scaleY = d3.scaleLinear().range([height - margin, margin]).domain([0, d3.max(data.map(d => Math.max(d[1], d[2])))]);
         // epa score scale doesnt vary based on max values but its always between 0 and 5
         if (trackType == 'entrustment_score') {
             scaleY = d3.scaleLinear().range([height - margin, margin]).domain([0, 5]);
         }
 
-        // create the line path
-        const line = d3.line().x(d => scaleX(data.findIndex(dd => dd == d))).y(d => scaleY(d[1]))(data);
 
-        // create circles
-        const circles = data.map((d, i) => {
-            return <circle
-                cx={scaleX(i)}
-                cy={scaleY(d[1])}
+        // create bars
+        const bars = data.map((d, i) => {
+            return <rect
+                x={scaleX(i)}
+                y={scaleY(d[1])}
+                height={height - margin - scaleY(d[1])}
                 fill={'#43b98e'}
-                r={5}
-                strokeWidth={2}
+                width={itemSize - (0.25 * itemSize)}
                 stroke={'transparent'}
-                key={d[0]}>
-                <title>{d[0] + '\nOverall: ' + (trackType == 'expired_epa_percentage' ? d[1] + '%' : d[1]) + '\nPeriod: ' + (trackType == 'expired_epa_percentage' ? d[2] + '%' : d[2])}</title>
-            </circle>
+                key={d[0]}
+                opacity={'0.50'}>
+            </rect>
         });
 
-        let lineInDateRange, circlesInDateRange;
+        let barsInDateRange;
 
         if (dateFilterActive && startDate && endDate) {
-            lineInDateRange = d3.line().x(d => scaleX(data.findIndex(dd => dd == d))).y(d => scaleY(d[2]))(data);
-            circlesInDateRange = data.map((d, i) => {
-                return <circle
-                    cx={scaleX(i)}
-                    cy={scaleY(d[2])}
+            barsInDateRange = data.map((d, i) => {
+                return <rect
+                    x={scaleX(i)}
+                    y={scaleY(d[2])}
+                    height={height - margin - scaleY(d[2])}
                     fill={'rgba(151,187,205,1)'}
-                    r={5}
-                    strokeWidth={2}
+                    width={itemSize - (0.25 * itemSize)}
                     stroke={'transparent'}
-                    key={d[0]}>
-                    <title>{d[0] + '\nOverall: ' + (trackType == 'expired_epa_percentage' ? d[1] + '%' : d[1]) + '\nPeriod: ' + (trackType == 'expired_epa_percentage' ? d[2] + '%' : d[2])}</title>
-                </circle>
+                    key={d[0]}
+                    opacity={'0.50'}>
+                </rect>
             });
         }
 
@@ -101,12 +101,10 @@ export default class FacultyGraph extends Component {
 
         // for entrustment score we have 6 lines and special axis texts
         if (trackType == 'entrustment_score') {
-
             axisTickLines = _.times(6, (index) => {
                 let verticalPosition = index;
                 return d3.line().x(d => d[0]).y(d => scaleY(d[1]))([[Xoffset, verticalPosition], [width - margin, verticalPosition]])
             })
-
             axisTickTexts = _.map(axisTickLines, (unused, index) => {
                 return <text key={'axis-tick' + index}
                     x={5} y={height - (margin / 2) - (index * ((height - (2 * margin)) / 5))} fontWeight='bold' fill='#a9a1a1'>
@@ -115,27 +113,35 @@ export default class FacultyGraph extends Component {
             });
         }
 
-
         const hoverLines = data.map((d, i) => {
             return <rect
-                key={i}
+                className={'hoverable-rectangle ' + (d[0] == currentFaculty ? 'faculty-rectangle' : '')}
                 x={scaleX(i)}
-                y={0}
-                height={height}
-                width={15}
+                y={margin - (d[0] == currentFaculty ? 10 : 0)}
+                height={height - (2 * margin) + (d[0] == currentFaculty ? 10 : 0)}
+                fill={'black'}
+                width={itemSize - (0.25 * itemSize)}
+                stroke={'transparent'}
+                key={d[0]}
                 opacity={0}>
                 <title>{d[0] + '\nOverall: ' + (this.props.trackType == 'expired_epa_percentage' ? d[1] + '%' : d[1]) + '\nPeriod: ' + (this.props.trackType == 'expired_epa_percentage' ? d[2] + '%' : d[2])}</title>
             </rect>
         });
 
+
+        const currentFacultyIndex = _.findIndex(data, (d) => d[0] == currentFaculty);
+
         return (<svg className='supervisor-line-chart' width={width} height={height} >
             <path d={axisTickLines} fill="none" stroke="#564d4d4d" strokeWidth="2px"></path>
-            <path d={line} fill="none" stroke="#43b98e" strokeWidth="2px"></path>
-            <g>{hoverLines}</g>
-            <g>{circles}</g>
-            <path d={lineInDateRange} fill="none" stroke="rgba(151,187,205,1)" strokeWidth="2px"></path>
-            <g>{circlesInDateRange}</g>
+            <g>{bars}</g>
+            <g>{barsInDateRange}</g>
             <g>{axisTickTexts}</g>
-        </svg>);
+            <g>{hoverLines}</g>
+            {(currentFacultyIndex != -1) &&
+                <text className='faculty-highlight-label'
+                    x={scaleX(currentFacultyIndex) + 10} y={15} >
+                    {data[currentFacultyIndex][1] + (trackType == 'expired_epa_percentage' ? '%' : '')}
+                </text>}
+        </svg >);
     }
 }
