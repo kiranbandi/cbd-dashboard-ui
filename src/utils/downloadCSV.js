e1cba8c2a1fe160cce671d594b3708f973ad2f99
Sean Wong <seanwong24@live.com>
2020-01-13 17:26:47 -0600

    fix EPA display issue and large file download issue for data export

--- src/utils/downloadCSV.js
+++ src/utils/downloadCSV.js
@@ -1,13 +1,14 @@
 import _ from 'lodash';
+import FileSaver from 'file-saver'
 
-export default function(columnNames = ['Date', 'Resident Name', 'EPA', 'Observer Name', 'Observer Type', 'Rating', 'Type', 'Situation Context', 'Feedback', 'Professionalism Safety', 'EPA Expired']) {
+export default function (columnNames = ['Date', 'Resident Name', 'EPA', 'Observer Name', 'Observer Type', 'Rating', 'Type', 'Situation Context', 'Feedback', 'Professionalism Safety', 'EPA Expired']) {
     var { rcmData = [] } = window.emCBD;
     // Escaping strings in quotes to evade fuckery because of
     // commas within the strings that can cause the csv file 
     // format to be changed
     var convertedData = _.map(rcmData, (dataPoint) => {
         return _.map(dataPoint, (value) => {
-            if (typeof(value) == 'string') {
+            if (typeof (value) == 'string') {
                 //  quick fix hashes seem to be breaking the code so we will replace them with enclosed text of hash
                 if (value.indexOf("#") > -1) {
                     value = value.split("#").join("-hash-");
@@ -19,11 +20,14 @@ export default function(columnNames = ['Date', 'Resident Name', 'EPA', 'Observer
 
     // Add file headers to top of the file
     convertedData.unshift(columnNames);
-    var csvContent = "data:text/csv;charset=utf-8," + convertedData.join("\n"),
-        encodedUri = encodeURI(csvContent),
-        link = document.createElement("a"),
-        timeStamp = (new Date()).toString().split("GMT")[0];
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", 'rcm-data' + "-" + timeStamp + ".csv");
-    link.click();
+    // var csvContent = "data:text/csv;charset=utf-8," + convertedData.join("\n"),
+    //     encodedUri = encodeURI(csvContent),
+    //     link = document.createElement("a"),
+    //     timeStamp = (new Date()).toString().split("GMT")[0];
+    // link.setAttribute("href", encodedUri);
+    // link.setAttribute("download", 'rcm-data' + "-" + timeStamp + ".csv");
+    // link.click();
+    var blob = new Blob([convertedData.join("\n")], { type: "text/csv;charset=utf-8" });
+    var timeStamp = (new Date()).toString().split("GMT")[0];
+    FileSaver.saveAs(blob, "rcm-data" + "-" + timeStamp + ".csv");
 }
