6042393d8cab96a484c33bbaef4a1fab63f7083b
Sean Wong <seanwong24@live.com>
2020-02-10 00:39:07 +0000

    modify EPA monthly rotation view

--- src/components/ProgramEvaluationGroup/EPAMonthlyRotation.jsx
+++ src/components/ProgramEvaluationGroup/EPAMonthlyRotation.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs';
 import moment from 'moment';
-import ReactSelect from 'react-select';
+import * as d3 from 'd3';
 
 const monthList = ["Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun"];
 
@@ -12,12 +12,6 @@ export default class EPAMonthlyRotation extends Component {
         this.state = {
             selectedAcademicYears: [props.defaultAcademicYear]
         };
-        this.onSelectChange = this.onSelectChange.bind(this);
-    }
-
-
-    onSelectChange(selectedAcademicYears) {
-        this.setState({ selectedAcademicYears });
     }
 
     render() {
@@ -41,13 +35,40 @@ export default class EPAMonthlyRotation extends Component {
             '#fdee00'
         ];
 
-        for (const academicYear of this.state.selectedAcademicYears) {
+        if (possibleAcademicYears) {
+            for (const academicYear of possibleAcademicYears) {
+                // Create empty map for all months
+                let monthCount = {};
+                // Count records for each month
+                const records = filteredRecords.filter(d => moment(d.observation_date, 'YYYY-MM-DD').isBetween(moment('07/01/' + Number(academicYear.value), 'MM/DD/YYYY'), moment('06/30/' + (Number(academicYear.value) + 1), 'MM/DD/YYYY'), 'days', '[]'));
+                _.map(records, (d) => {
+                    let monthKey = moment(d.observation_date, 'YYYY-MM-DD').format('MMM');
+                    if (monthCount.hasOwnProperty(monthKey)) {
+                        monthCount[monthKey] += 1;
+                    }
+                    else {
+                        monthCount[monthKey] = 1;
+                    }
+                });
+
+                if (d3.sum([...Object.values(monthCount)]) > 0) {
+                    lineData.datasets.push({
+                        label: academicYear.label,
+                        fillColor: "rgba(28,168,221,.03)",
+                        strokeColor: colors[lineData.datasets.length],
+                        pointColor: colors[lineData.datasets.length],
+                        pointStrokeColor: 'rgba(28,168,221,.03)',
+                        pointHighlightFill: "rgba(28,168,221,.03)",
+                        pointHighlightStroke: "rgba(220,220,220,1)",
+                        data: _.map(monthList, (d) => monthCount[d] || 0)
+                    });
+                }
+            }
+        } else {
             // Create empty map for all months
             let monthCount = {};
             // Count records for each month
-            const records = academicYear ?
-                filteredRecords.filter(d => moment(d.observation_date, 'YYYY-MM-DD').isBetween(moment('07/01/' + Number(academicYear.value), 'MM/DD/YYYY'), moment('06/30/' + (Number(academicYear.value) + 1), 'MM/DD/YYYY'), 'days', '[]')) :
-                filteredRecords;
+            const records = filteredRecords;
             _.map(records, (d) => {
                 let monthKey = moment(d.observation_date, 'YYYY-MM-DD').format('MMM');
                 if (monthCount.hasOwnProperty(monthKey)) {
@@ -57,7 +78,6 @@ export default class EPAMonthlyRotation extends Component {
                     monthCount[monthKey] = 1;
                 }
             });
-
             lineData.datasets.push({
                 label: "Rotations",
                 fillColor: "rgba(28,168,221,.03)",
@@ -78,29 +98,12 @@ export default class EPAMonthlyRotation extends Component {
             <div className='col-sm-6 col-xs-12 reel-in-left epa-specific'>
                 <div className='m-a program-vis-box row'>
                     <h3 className='text-left m-b'>EPA Monthly Distribution</h3>
-                    <div className="epa-select m-a text-left">
-                        <span className='inner-span'>Select EPA</span>
-                        <div className='react-select-root'>
-                            <ReactSelect
-                                value={this.state.selectedAcademicYears}
-                                options={possibleAcademicYears}
-                                isMulti={true}
-                                styles={{
-                                    option: (styles) => ({
-                                        ...styles,
-                                        color: 'black',
-                                        textAlign: 'left'
-                                    })
-                                }}
-                                onChange={this.onSelectChange} />
-                        </div>
-                    </div>
 
                     <div className='col-xs-12'>
                         <Line
                             options={lineOptions}
                             data={lineData}
-                            width={width} height={350}
+                            width={width} height={400}
                             redraw={true} />
                     </div>
                 </div>
