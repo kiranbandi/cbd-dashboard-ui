4a81393e02bb55ae75fa5b98459a6b58f65c352b
kiranbandi <bvenkatkiran@gmail.com>
2020-07-01 14:20:48 -0600

    y2k style rotation schedule roll over bug

---
+++ src/utils/infoTooltipReference.js
@@ -0,0 +1,5 @@
+var reference = {
+    'pc_acquired_expired': 'This chart shows the total number of EPAs that have been acquired (completed and not expired) by residents in each program and the total number of EPAs that have expired. It also show the average number of EPAs that are acquired and expired per resident in each program.'
+}
+
+module.exports = reference;
