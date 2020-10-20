ce5049c99d7459caf856169d06a7d4ce37911a3c
kiranbandi <bvenkatkiran@gmail.com>
2020-10-19 19:40:53 -0600

    moved radar chart into indepedant component

--- src/components/FacultyDashbordGroup/FacultyInfoGroup.jsx
+++ src/components/FacultyDashbordGroup/FacultyInfoGroup.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import FacultyStatCardSet from './FacultyStatCardSet';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, } from 'recharts';
-
+import FacultyRadarChart from './FacultyRadarChart';
 
 export default (props) => {
 
     const { isUG = false, width, currentRotation, currentFaculty, dateFilterActive,
         processedRecords, currentFacultyRecords, programInfo } = props;
-
-    let radarChartWidth = width - 1100 > 400 ? 400 : width / 2,
-        radarRadius = (radarChartWidth / 2) - 30;
-
-    let epaList = getEPAList(programInfo.epaSourceMap),
-        overallepaGroupList = _.map(processedRecords, (d) => d.epaGroup),
-        currentFacultyepaGroupList = currentFacultyRecords.length > 0 ? currentFacultyRecords[0].epaGroup : [],
-        overallepaGroupSummed = sumArrays(...overallepaGroupList),
-        totalMax = _.sumBy(epaList, (e) => e.max),
-        currentFacultySumCount = _.sum(currentFacultyepaGroupList) == 0 ? 1 : _.sum(currentFacultyepaGroupList),
-        totalSumCount = _.sum(overallepaGroupSummed),
-        newData = _.map(epaList, (epa, epaIndex) => {
-            return {
-                'label': epa.label,
-                'overall': (+overallepaGroupSummed[epaIndex] / totalSumCount) / (+epa.max / totalMax),
-                'currentFaculty': ((currentFacultyepaGroupList[epaIndex] || 0) / currentFacultySumCount) / (+epa.max / totalMax),
-                'max': epa.max
-            };
-        });
 
     return <div className='text-center'>
         <div className='print-info' style={{ 'display': 'inline-block', 'width': '1120px' }}>
@@ -41,52 +21,10 @@ export default (props) => {
                 processedRecords={currentFaculty == 'ALL' ? [] : currentFacultyRecords}
                 dateFilterActive={dateFilterActive} />
         </div>
-        <div className='faculty-radar-chart'>
-            <div className="hr-divider">
-                <h4 className="hr-divider-content"> EPA Distribution </h4>
-            </div>
-            {overallepaGroupSummed.length > 0 &&
-                <RadarChart cx={radarRadius + 35} cy={radarRadius + 25}
-                    outerRadius={radarRadius}
-                    width={radarChartWidth} height={radarChartWidth} data={newData}>
-                    <PolarGrid />
-                    <PolarAngleAxis dataKey="label" />
-                    <Radar name="Overall" dataKey="overall" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-                    {currentFacultyRecords.length > 0
-                        && <Radar name="CurrentFaculty" dataKey="currentFaculty" stroke="#1ca8dd" fill="#1ca8dd" fillOpacity={0.6} />}
-                </RadarChart>}
-        </div>
+        <FacultyRadarChart
+            width={width}
+            processedRecords={processedRecords}
+            currentFacultyRecords={currentFacultyRecords}
+            programInfo={programInfo} />
     </div>
-
-}
-
-
-function getEPAList(epaSourceMap) {
-    // create a epa list to map epa distribution
-    let templateEpaSourceMap = _.cloneDeep(epaSourceMap),
-        EPAList = [];
-
-    // if sub root is available on first level then its UG
-    //  so do a shallow loop
-    if (templateEpaSourceMap.hasOwnProperty('subRoot')) {
-        _.map(templateEpaSourceMap.subRoot, (epa, epaKey) => {
-            EPAList.push({ 'label': epaKey, 'max': templateEpaSourceMap['maxObservation'][epaKey] });
-        });
-    } else {
-        // remove special assessment EPAs if any
-        _.map(templateEpaSourceMap, (epaSource, key) => {
-            _.map(epaSource.subRoot, (epa, epaKey) => {
-                if (epa.indexOf('(SA)') == -1) {
-                    EPAList.push({ 'label': epaKey, 'max': epaSource['maxObservation'][epaKey] });
-                }
-            })
-        });
-    }
-    return EPAList;
-}
-
-function sumArrays(...arrays) {
-    const n = arrays.reduce((max, xs) => Math.max(max, xs.length), 0);
-    const result = Array.from({ length: n });
-    return result.map((_, i) => arrays.map(xs => xs[i] || 0).reduce((sum, x) => sum + x, 0));
 }
---
+++ src/components/FacultyDashbordGroup/FacultyRadarChart.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, } from 'recharts';
+
+export default (props) => {
+
+    const { width, processedRecords, currentFacultyRecords, programInfo } = props;
+
+    let radarChartWidth = width - 1100 > 400 ? 400 : width / 2,
+        radarRadius = (radarChartWidth / 2) - 30;
+
+    let epaList = getEPAList(programInfo.epaSourceMap),
+        overallepaGroupList = _.map(processedRecords, (d) => d.epaGroup),
+        currentFacultyepaGroupList = currentFacultyRecords.length > 0 ? currentFacultyRecords[0].epaGroup : [];
+
+    let trainingStageGroupMax = {};
+    // For every training stage get the corresponding maximum count
+    // from EPA list then from overall list and current faculty list 
+    _.map(epaList, (d, epaListIndex) => {
+        const trainingStageKey = d.label[0];
+        if (trainingStageGroupMax.hasOwnProperty(trainingStageKey)) {
+            const currentKey = trainingStageGroupMax[trainingStageKey];
+            // update each individual epa value into count
+            currentKey.max.push(d.max);
+            currentKey.epaCount.push(_.sumBy(overallepaGroupList, (d) => +d[epaListIndex]) || 0),
+                currentKey.currentFacultyCount.push(currentFacultyepaGroupList[epaListIndex] || 0)
+        }
+        else {
+            trainingStageGroupMax[trainingStageKey] = {
+                max: [d.max],
+                epaCount: [_.sumBy(overallepaGroupList, (d) => +d[epaListIndex]) || 0],
+                currentFacultyCount: [currentFacultyepaGroupList[epaListIndex] || 0]
+            }
+        }
+    });
+
+    // Now for every training stage calculate the overall sum
+    _.map(trainingStageGroupMax, (t) => {
+        t.max = _.sum(t.max);
+        t.epaCount = _.sum(t.epaCount);
+        t.currentFacultyCount = _.sum(t.currentFacultyCount);
+    })
+
+    let overallepaGroupSummed = sumArrays(...overallepaGroupList);
+
+
+    let newData = _.map(epaList, (epa, epaIndex) => {
+
+        let trainingStageMax = trainingStageGroupMax[epa.label[0]];
+
+        return {
+            'label': epa.label,
+            'overall': (+overallepaGroupSummed[epaIndex] / trainingStageMax.epaCount) / (+epa.max / trainingStageMax.max),
+            'currentFaculty': ((currentFacultyepaGroupList[epaIndex] || 0) / trainingStageMax.currentFacultyCount) / (+epa.max / trainingStageMax.max)
+        };
+    });
+
+
+
+
+
+    return <div className='faculty-radar-chart'>
+        <div className="hr-divider">
+            <h4 className="hr-divider-content"> EPA Distribution </h4>
+        </div>
+        {overallepaGroupSummed.length > 0 &&
+            <RadarChart cx={radarRadius + 35} cy={radarRadius + 25}
+                outerRadius={radarRadius}
+                width={radarChartWidth} height={radarChartWidth} data={newData}>
+                <PolarGrid />
+                <PolarAngleAxis dataKey="label" />
+                <Radar name="Overall" dataKey="overall" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                {currentFacultyRecords.length > 0
+                    && <Radar name="CurrentFaculty" dataKey="currentFaculty" stroke="#1ca8dd" fill="#1ca8dd" fillOpacity={0.6} />}
+            </RadarChart>}
+    </div>
+}
+
+
+function getEPAList(epaSourceMap) {
+    // create a epa list to map epa distribution
+    let templateEpaSourceMap = _.cloneDeep(epaSourceMap),
+        EPAList = [];
+
+    // if sub root is available on first level then its UG
+    //  so do a shallow loop
+    if (templateEpaSourceMap.hasOwnProperty('subRoot')) {
+        _.map(templateEpaSourceMap.subRoot, (epa, epaKey) => {
+            EPAList.push({ 'label': epaKey, 'max': templateEpaSourceMap['maxObservation'][epaKey] });
+        });
+    } else {
+        // remove special assessment EPAs if any
+        _.map(templateEpaSourceMap, (epaSource, key) => {
+            _.map(epaSource.subRoot, (epa, epaKey) => {
+                if (epa.indexOf('(SA)') == -1) {
+                    EPAList.push({ 'label': epaKey, 'max': epaSource['maxObservation'][epaKey] });
+                }
+            })
+        });
+    }
+    return EPAList;
+}
+
+function sumArrays(...arrays) {
+    const n = arrays.reduce((max, xs) => Math.max(max, xs.length), 0);
+    const result = Array.from({ length: n });
+    return result.map((_, i) => arrays.map(xs => xs[i] || 0).reduce((sum, x) => sum + x, 0));
+}
