0382b516071e0c845dc94edd3683d1249dacae5e
kiranbandi <bvenkatkiran@gmail.com>
2020-11-23 04:15:44 -0600

    Assessment plans for Anesthesia, GIM and Path addedd

--- src/utils/programInfo/programs/GIM.js
+++ src/utils/programInfo/programs/GIM.js
@@ -36,6 +36,14 @@ const programInfo = {
                 '1.1': 1,
                 '1.2': 2,
             },
+            assessmentInfo: {
+                '1.1': `Collect one direct observation by GIM attending.`,
+                '1.2': `Part A:
+                Submit evidence of active ACLS certification.
+
+                Part B:
+                One direct observation by attending staff.`,
+            },
             clinicalPresentation: {},
             patientDemographic: {},
             filterTitles: {},
@@ -56,6 +64,18 @@ const programInfo = {
                 '2.1': 4,
                 '2.2': 4,
                 '2.3': 4
+            },
+            assessmentInfo: {
+                '2.1': `Collect at least 4 observations of achievement
+                - At least 2 high case complexity
+                - At least 2 assessors`,
+                '2.2': `Collect at least 4 observations of achievement: 
+                - At least 3 different conditions`,
+                '2.3': `Collect 4 observations of achievement
+                - At least 3 GIM clinic
+                - At least 2 medium complexity
+                - Range of decisions
+                - At least 2 assessors`
             },
             clinicalPresentation: {
                 '2.1': ["low", "medium", "high"],
@@ -124,6 +144,112 @@ const programInfo = {
                 '3.17': 4,
                 '3.18': 1,
                 '3.19': 22
+            },
+            assessmentInfo: {
+                '3.1': `Collect at least 6 observations of achievement
+                - A range of complexity
+                - At least 2 of each stage of care`,
+                '3.2': `Collect at least 10 observations of achievement
+                - At least 4 undifferentiated presentations
+                - At least 2 newly diagnosed conditions
+                - At least 4 chronic medical conditions
+                - At least 4 assessors`,
+                '3.3': `Part A: Collect 5 observations of achievement
+                - At least 2 urgent
+                - All medium or high complexity.
+                
+                Part B: Collect 5 observations of achievement
+                - At least 4 medium or high complexity
+                - At least 2 with unstable clinical status`,
+                '3.4': `Part A: Collect 6 observations of achievement
+                - At least 2 inpatient
+                - At least 3 each antepartum and postpartum
+                - At least 4 different diagnoses
+                - At least one diabetes
+                - At least one hypertension.
+                
+                Part B: Collect 2 observations of achievement.`,
+                '3.5': `Part A: Collect 1 observation of achievement.
+                
+                Part B: Collect 2 observations of achievement 
+                - At least one outpatient.`,
+                '3.6': `Part A: Collect 2 observations
+                - At least one behavioural change.
+                
+                Part B: Collect 2 observations of achievement.`,
+                '3.7': `Collect 3 observations of achievement
+                - At least one transition from disease-modifying therapy to symptom management
+                - At least one transition to end of life care.`,
+                '3.8': `Part A: Collect 8 observations of achievement
+                - At least two invasive ventilation
+                - At least two non-invasive ventilation
+                - At least two patients on vasopressor support
+                - No more than two that required transfer.
+                
+                Part B: Collect 1 observation of achievement.
+                
+                Part C: Collect feedback from at least 3 observers
+                - At least one supervisor
+                - At least one nurse or other health care professional.`,
+                '3.9': `Collect 5 observations of achievement
+                - At least one of each type of document
+                - At least 2 different assessors.`,
+                '3.10': `Collect 3 observations of achievement
+                - At least one with patient/family
+                - At least one with other health care professional(s).`,
+                '3.11': `Part A: Collect on observation of achievement 
+                - Must be a mix of studies
+                
+                Part B: Competence committee reviews resident’s procedure log.
+                
+                Part C: Supervision and interpretation of exercise stress tests.
+                Collect 10 observations of achievement.`,
+                '3.12': ` Part A: Collect feedback from at least 6 observers
+                - At least one supervisor
+                - At least one nurse or other health care professional.
+                
+                Part B: Direct and/or indirect observation by supervisor of a week of clinical activity.
+                Collect 2 observations of achievement.`,
+                '3.13': `Collect 2 observations
+                - At least one day of high complexity.`,
+                '3.14': `Part A: Collect one observation of achievement.
+                
+                Part B: Collect one observation of achievement.
+                
+                Part C: Collect at least one observation of achievement
+                ---	if supervisor including input from at least 3 individuals or
+                ---	if multisource feedback - feedback from at least 3 individuals`,
+                '3.15': `Part A: Collect 4 observations of achievement
+                -	At least one of each type of teaching 
+                - At least two different assessors
+                
+                Part B: Collect 4 observations of achievement.
+                
+                Part C: Collect feedback from at least 6 learners
+                - At least one medical student
+                - At least one junior resident.`,
+                '3.17': `Collect 4 observations of achievement
+                - At least one review of consult note/written communication to other MD
+                - At least one direct observation of communication with patient.`,
+                '3.19': `Part A: Collect 21 observations of achievement
+                - At least one of each procedure in the clinical setting
+                - At least 5 central line placements using ultrasound guidance
+                --- Must include different sites
+                - At least 3 thoracentesis using ultrasound guidance
+                - At least 3 paracentesis using ultrasound guidance
+                - At least 3 lumbar puncture
+                - At least 2 knee arthrocentesis
+                - At least 3 airway management (bag and mask ventilation) and endotracheal
+                intubations
+                - At least 2 arterial line catheter insertions
+                
+                Part B: Log to track
+                -	Setting: clinical; simulation
+                -	Procedure: airway management & endotracheal intubation; arterial line catheter
+                insertion; central line placement; thoracentesis; paracentesis; lumbar puncture;
+                knee arthrocentesis
+                - Site: not applicable; femoral; internal jugular; subclavian; knee
+                - Use of ultrasound guidance: yes; no`
             },
             clinicalPresentation: {
                 '3.1': ["social", "undifferentiated condition", "multi-system"],
@@ -197,6 +323,11 @@ const programInfo = {
             maxObservation: {
                 '4.1': 3,
                 '4.2': 1,
+            },
+            assessmentInfo: {
+                '4.1': `Collect 3 observations of achievement.
+                - At least one that includes after hours coverage.`,
+                '4.2': `Collect 1 successful observation`
             },
             clinicalPresentation: {},
             patientDemographic: {},
