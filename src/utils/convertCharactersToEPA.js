9f4a30172650e275c513db4058ce7e283d6bbf69
kiranbandi <bvenkatkiran@gmail.com>
2021-01-12 17:47:39 -0600

    updated EPA labelling to follow textual format instead of decimal styling

--- src/utils/convertCharactersToEPA.js
+++
@@ -1,6 +0,0 @@
-export default (epa) => {
-    let epaParts = epa.split('.');
-    return ((epaParts[0] == 'D' ? '1' :
-        epaParts[0] == 'F' ? '2' :
-            epaParts[0] == 'C' ? '3' : 'P') + '.' + epaParts[1]);
-}; 
