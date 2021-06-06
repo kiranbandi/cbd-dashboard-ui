5a30997dc14f473f36d25cf0b52f7ec67e736a3f
kiranbandi <bvenkatkiran@gmail.com>
2021-06-05 22:13:05 -0600

    updated API endpoint to match elentra 1.21

--- src/utils/endPoints.js
+++ src/utils/endPoints.js
@@ -1,6 +1,6 @@
 var endPoints = {
     // end points to get learner related info
-    learners: ENTRADA_URL + "/assessments/learners",
+    learners: ENTRADA_URL + "/assessments",
 }
 
 module.exports = endPoints;
