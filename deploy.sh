97cd12bebdb29177b7e207e085f61e686de50aaa
kiranbandi <bvenkatkiran@gmail.com>
2022-03-16 07:27:08 -0600

    removed node-sass

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 # Script to update apache server
 git pull origin master
-# install dependencies
-sudo npm i
-#force install node-sass
-sudo npm install node-sass
 # create new build folder
 npm run build
 # stop nginx  server
