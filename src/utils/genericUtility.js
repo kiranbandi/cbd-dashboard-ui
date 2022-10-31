7d5f707195bfae88b1e65172a749c7c3d619e133
kiranbandi <bvenkatkiran@gmail.com>
2022-10-30 22:10:10 -0600

    force string typecasting for lowercase conversion

--- src/utils/genericUtility.js
+++ src/utils/genericUtility.js
@@ -3,7 +3,7 @@ var utility = {};
 utility.customFilter = function(filter, rows) {
     rows[filter.id] = rows[filter.id] || '';
     filter.value = filter.value || '';
-    return rows[filter.id].toLowerCase().indexOf(filter.value.toLowerCase()) > -1;
+    return String(rows[filter.id]).toLowerCase().indexOf(String(filter.value).toLowerCase()) > -1;
 }
 
 module.exports = utility;
