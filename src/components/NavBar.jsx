6ff643ebbe3762834e4a8ed125447771f3eb8e4f
Venkat Bandi <bvenkatkiran@gmail.com>
2025-10-23 14:45:10 -0600

    updating cas redirect path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ class NavBar extends Component {
     logOut(event) {
         event.preventDefault();
         this.props.setLogoutData();
-        location.href = 'https://pawscas.usask.ca/cas-web/logout?url=' + encodeURIComponent(window.location.origin + '/');
+        location.href = 'https://pawscas.usask.ca/cas-web/logout?url=' + encodeURIComponent(window.location.href);
     }
 
 
@@ -40,7 +40,7 @@ class NavBar extends Component {
         const { userDetails, onProgramChange } = this.props,
             { accessType = '', program, programList = [] } = userDetails,
             isUG = (program == 'UNDERGRADUATE'),
-            loginRedirectURL = 'https://cas.usask.ca/cas/login?service=' + encodeURIComponent(window.location.origin + '/');
+            loginRedirectURL = 'https://cas.usask.ca/cas/login?service=' + encodeURIComponent(window.location.href);
 
         // pass in the program list that the user has access to so that he can switch between
         // them and ask for a different token
--- src/utils/requestServer.js
+++ src/utils/requestServer.js
@@ -508,7 +508,7 @@ function errorCallback(error, reject) {
     if (error.response && error.response.data) {
         // if its an authorization error, we simply redirect
         // the users to the paws login page
-        let loginRedirectURL = 'https://cas.usask.ca/cas/login?service=' + encodeURIComponent(window.location.origin + '/');
+        let loginRedirectURL = 'https://cas.usask.ca/cas/login?service=' + encodeURIComponent(window.location.href);
         if (error.response.status == 401) {
             //  handle token expiry gracefully and log the user back in 
             // with minimal effort.
